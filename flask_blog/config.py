28950c55fa910857cf4fe12d5d53e4e3a76be454
Puru Chaudhary <puru.chaudhary17@hotmail.com>
2024-03-03 17:28:08 -0800

    setting up deploy branch, with production deployment changes

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,10 +1,14 @@
 import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USER')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
+    MAIL_USERNAME = config.get('MAIL_USER')
+    MAIL_PASSWORD = config.get('MAIL_PASS')
